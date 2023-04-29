6c1a6fd3c5d9179704e50972bbcdb58d669bcece
Elad Magen <eladmagen10@gmail.com>
2023-04-29 11:44:02 +0300

    Added main console and EXIT command

--- Trivia/Trivia/Main.cpp
+++ Trivia/Trivia/Main.cpp
@@ -1,17 +1,18 @@
 #pragma comment (lib, "ws2_32.lib")
-
 #include "WSAInitializer.h"
 #include "Server.h"
 #include <iostream>
 #include <exception>
 int main()
 {
+    std::string input = "";
     WSAInitializer wsaInit;
     Server server;
     server.run();
-    while(true)
+    while(input != "EXIT")
     {
-
+        input = "";
+        std::cin >> input;
     }
     return 0;
 }
