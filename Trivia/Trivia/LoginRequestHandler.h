b9abd5f64682e44b3d16329c1da903aafe853507
Elad Magen <eladmagen10@gmail.com>
2023-04-30 19:19:34 +0300

    added virtual func to IRH and override in LRH

--- Trivia/Trivia/LoginRequestHandler.h
+++ Trivia/Trivia/LoginRequestHandler.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "IRequestHandler.h"
+#include "Request.h"
 class LoginRequestHandler : public IRequestHandler
 {
-
+public:
+	virtual bool isRequestRelevant(RequestInfo) override;
+	virtual RequestResult handleRequest(RequestInfo) override;
 };
--- Trivia/Trivia/Request.h
+++ Trivia/Trivia/Request.h
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include "IRequestHandler.h"
+#include "Communicator.h"
 
 struct LoginRequest
 {
@@ -12,3 +15,21 @@ struct SignUpRequest
 	std::string password;
 	std::string email;
 }typedef SignUpRequest;
+
+
+struct RequestResult
+{
+	std::vector<unsigned char> response;
+	IRequestHandler* newHandler;
+}typedef RequestResult;
+
+
+
+struct RequestInfo
+{
+	unsigned char id;
+	std::vector<byte> buffer;
+}typedef RequestInfo;
+
+
+
