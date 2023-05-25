80acf169775121d002efc57b33b643ee612fb093
Elad Magen <eladmagen10@gmail.com>
2023-05-25 18:45:35 +0300

    Added more windows and pictures

--- Client/Client/Login.xaml.cs
+++ Client/Client/Login.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 
 namespace Client
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-           Signup signup = new Signup();
-           signup.ShowDialog();
         }
     }
 }
--- Client/Client/Signup.xaml.cs
+++ Client/Client/Signup.xaml.cs
@@ -10,26 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-
 
 namespace Client
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Signup.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Signup : Window
     {
-        public MainWindow()
+        public Signup()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-           Signup signup = new Signup();
-           signup.ShowDialog();
         }
     }
 }
