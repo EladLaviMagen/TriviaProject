55c0a32a572c9ed5a4bb739224e74a5b730eef0f
maayan zefania <maayanzefania@gmail.com>
2023-06-19 19:51:53 +0300

    added two screens

---
+++ Client/Client/Game.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for Game.xaml
+    /// </summary>
+    public partial class Game : Window
+    {
+        public Game()
+        {
+            InitializeComponent();
+        }
+        private void ans1_clicked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ans2_clicked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ans3_clicked(object sender, RoutedEventArgs e)
+        {
+        }
+        private void ans4_clicked(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
